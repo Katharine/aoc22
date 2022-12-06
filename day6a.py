d8d1291f916f6f9c7a156521c670adc85ee5278b
Katharine Berry <ktbry@google.com>
2022-12-05 21:07:36 -0800

    Day 6.

---
+++ day6a.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Google LLC.
+# SPDX-License-Identifier: Apache-2.0
+
+with open('day6.dat') as f:
+    text = f.readline().strip()
+
+last_four = ''
+for i in range(len(text)):
+    last_four += text[i]
+    if len(set(last_four)) == 4:
+        print(i)
+        break
+    if len(last_four) >= 4:
+        last_four = last_four[1:]
